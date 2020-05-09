43252e2bc35d1fc36ff514edb13e3cd5fff13421
bulbulahmed9 <bulbulahmed.tk@gmail.com>
2020-05-09 18:14:47 +0600

    update user phone route done

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,6 +1,7 @@
 const User = require('../model/userModel')
 
 
+// get user profile
 const getProfile = async (req, res, next) => {
     try {
       const {id} = req.cookies.mycookie.user
@@ -9,6 +10,32 @@ const getProfile = async (req, res, next) => {
     } catch (err) {
       console.log(err.message)
     }
-  }
+}
 
-  module.exports = getProfile
+// update user phone
+const updatePhone = async(req, res) => {
+    try {
+        const {id} = req.cookies.mycookie.user
+        const {phone} = req.body
+        if(!phone){
+            return res.json({
+                 msg: 'please provide a phone number'
+             })
+         }
+         
+         const user = await User.findById({_id: id})
+         user.phone = phone;
+         await user.save();
+         res.json({
+             msg: 'Phone updated'
+         })
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+// update user profile
+
+
+
+  module.exports = {getProfile, updatePhone}
--- server/routes/profileRoute.js
+++ server/routes/profileRoute.js
@@ -2,13 +2,19 @@ const router = require('express').Router()
 
 const auth = require('../middleware/auth')
 
-const getProfile = require('../controllers/profileController')
+const {getProfile, updatePhone} = require('../controllers/profileController')
 
 // route get /api/user/profile
 // desc get user profile
 // access // private
 
 router.get('/api/user/profile',auth, getProfile)
+
+// route get /api/user/profile/updatePhone
+// desc update user phone
+// access // private
+
+router.put('/api/user/profile/updatePhone',auth, updatePhone)
 
 
 module.exports = router;
