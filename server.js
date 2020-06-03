047bdf0507621b0cda6a9e51dec75a097cb73033
bulbulahmed9 <bulbulahmed.tk@gmail.com>
2020-06-03 19:41:10 +0600

    error fixed

--- server.js
+++ server.js
@@ -56,7 +56,9 @@ app.use((req, res, next) => {
 })
 app.use((error, req, res, next) => {
     res.status(error.status || 500)
-    res.send(`<h3> ${error.message} </h3> <br/> <a href="https://sellbd.herokuapp.com">Back to Home</a> `)
+    res.json({
+        msg: error.message
+    })
 })
 
 // Start the Server
